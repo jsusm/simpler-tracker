c6c790bfc5cf8b24941c93c85a4adaab480816d8
Jesus Marcano <jesusjaviermarcanomora.2005@gmail.com>
2026-05-09 10:51:08 -0400

    feat: formatting a partial linting
    there are some linting errors but they are for demo files that will be
    erased soon

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { defineConfig } from "vite";
+import { devtools } from "@tanstack/devtools-vite";
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
-import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
-import neon from './neon-vite-plugin.ts'
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+import neon from "./neon-vite-plugin.ts";
 
 const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
-    neon,
-    tailwindcss(),
-    tanstackStart(),
-    viteReact(),
-    babel({ presets: [reactCompilerPreset()] }),
-  ],
-})
+	resolve: { tsconfigPaths: true },
+	plugins: [
+		devtools(),
+		cloudflare({ viteEnvironment: { name: "ssr" } }),
+		neon,
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
+});
 
-export default config
+export default config;
