ad8220ecffd37d47f57349d293d93f3af1fe50ea
Jesus Marcano <jesusjaviermarcanomora.2005@gmail.com>
2026-05-09 11:09:42 -0400

    feat: migrate drizzle connection from pg to neon

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,9 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { neon } from '@neondatabase/serverless'
+import { drizzle } from "drizzle-orm/neon-http";
+import { env } from "#/env.ts"
+
+const sql = neon(env.DATABASE_URL);
 
 import * as schema from "./schema.ts";
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle({ client: sql, schema });
